0bd64e065f6b3656facc1063553394042a44c615
Tom Irimescu <tom@irimescu.com>
2020-02-09 14:39:14 -0500

    Install Tailwind SCSS, removed Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import {
 
 @Component({
   selector: 'app-root',
-  template: `<div class="bg-success p-2 text-center text-white">
+  template: `<div class="bg-green-500 p-3 text-center text-white text-2xl">
                   This is irimescu.com
             </div>
             <br />
-            <i class="fa fa-shopping-cart"></i>
+            <i class="fa fa-shopping-cart p-8"></i>
             <br />
-            <button class="btn btn-primary">Test Button</button>`
+            <button class="btn">Test Button</button>`
 })
 export class AppComponent {
   title = 'irimescu.com';
