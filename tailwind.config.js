3ad735a13b48fd9facad0b9b949bc3048e699876
Tom Irimescu <tom@irimescu.com>
2020-03-14 16:05:33 -0400

    Theme work

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        indigo: {
+          lighter: '#b3bcf5',
+          default: '#5c6ac4',
+          dark: '#202e78',
+        },
+        blue: {
+          lighter: '#3f51b5',
+          default: '#303f9f',
+          dark: '#1a237e',
+        },
+        red: {
+          lighter: '#ec4079',
+          default: '#e91e62',
+          dark: '#62155e',
+        }
+      }
     },
   },
   variants: {},
