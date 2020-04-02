134d47ec8188b7f419432fc23256fd8df970507a
Tom Irimescu <tom@irimescu.com>
2020-04-02 17:31:31 -0400

    Add test data to admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -5,6 +5,8 @@ import {
 import {
   Router
 } from '@angular/router';
+import { Product } from '@app/models/product/product.model';
+import { ProductRepository } from '@app/models/product/product.repository';
 
 @Component({
   selector: 'app-admin',
@@ -13,7 +15,13 @@ import {
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private repository: ProductRepository,
+    private router: Router) { }
+
+  get products(): Product[] {
+    return this.repository.getProducts();
+  }
 
   logout() {
     localStorage.removeItem('authJwtToken');
